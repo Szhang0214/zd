e541fc470df3ccdc221ce419a4693c6c9b6cd274
xueleixi <15376729817@163.com>
2017-12-01 14:15:28 +0800

    x

--- populate_nfy.js
+++ populate_nfy.js
@@ -5,45 +5,31 @@
  */
 var fs = require('fs');
 var iconv = require('iconv-lite');
-var file = '/Users/xueleixi/Desktop/bill/1.csv';
+var file = 'bills.csv';
 var process = require('process');
 
 var bytes = fs.readFileSync(file);
 var content = iconv.decode(bytes, 'gbk');
 var lines = content.split("\r\n");//客户名称
+var header = {};
 
-
-// printHeader(lines);
+extractHeader(lines);
+printHeader(lines);
 extend_Date();
-// console.log(new Date().Format("yyyy.MM.dd"));
-
 // process.exit(1);
 
-//第一行是表头，没用 最后一行是空行没用
-lines.splice(0, 1);
-lines.splice(lines.length - 1, 1);
-console.log(lines);
+//第一行是表头 第二行是占位符
+var headerLine=lines.splice(0, 1);
+var replaceCharLine=lines.splice(0, 1);
+
+lines.forEach(function (line, idx) {
+    if (line.trim() == '') {
+        lines.splice(idx, 1);
+    }
+});
+console.log(lines)
 
 var gRows = [];
-fields = {
-    0: "用户名",
-    1: "先生/女士",
-    2: "出借编号",
-    3: "产品名称",
-    4: "利润率",
-    5: "初始出借日期",
-    6: "初始出借金额",
-    7: "报告周期（开始时间）",//change
-    8: "报告周期（结束时间）",//change
-    9: "报告日期",//change
-    10: "报告日资产总量",//change
-    11: "",
-    12: "报告期内借款人应还款金额",
-    13: "报告日内您选择受让的债权金额",
-    14: "报告日您选择收回的金额",
-    15: "报告期内实际收益",
-
-};
 var pos_lent_money = 6;//初始出借金额
 var pos_rate = 4;//利润率
 var pos_report_end_date = 8;//报告周期（结束时间）
@@ -58,25 +44,22 @@ function compute_total_money(line) {
     var d1 = new Date(line[pos_lent_date].trim());
     var d2 = new Date(line[pos_report_end_date].trim());
     //计算投了多少个月了
-    var months=d2.getYear()*12+d2.getMonth()-d1.getYear()*12-d1.getMonth();
+    var months = d2.getYear() * 12 + d2.getMonth() - d1.getYear() * 12 - d1.getMonth();
     var rate = parseFloat(line[pos_rate]) / 100;//12%
     var profit = line[pos_lent_money] * rate / 12 * months;
-    line[pos_total_money] = parseInt(line[pos_lent_money])+ profit;
+    line[pos_total_money] = parseInt(line[pos_lent_money]) + profit;
     // return {m1: m1, m2: m2, rate: rate};
 }
 
 for (var i = 0; i < lines.length; i++) {
     var line = lines[i].split(',');
-    line.forEach(function (v,k) {
-        line[k]=v.trim();
+    line.forEach(function (v, k) {
+        line[k] = v.trim();
     })
     compute_total_money(line);
     gRows.push(line.join(','));
     //生成每个月的数据
     var period = 0;
-    if (line[pos_product] == '年丰盈') {
-        period = 12;
-    }
     switch (line[pos_product]) {
         case '年丰盈':
             period = 12;
@@ -88,8 +71,8 @@ for (var i = 0; i < lines.length; i++) {
             period = 0;
     }
     //第一个月的已经有了
-    for (var i = 1; i < period-1; i++) {
-        var newLine = line.slice();
+    for (var i = 1; i < period - 1; i++) {
+        var newLine = line.slice();//数组复制
         var oldDate = new Date(newLine[pos_report_start_date]);
         // 月数增加
         oldDate.setMonth(oldDate.getMonth() + i);
@@ -98,7 +81,7 @@ for (var i = 0; i < lines.length; i++) {
         newLine[pos_report_end_date] = oldDate.format('yyyy.MM.dd');
 
         var oldReportDate = new Date(newLine[pos_report_date]);
-        oldReportDate.setMonth(oldDate.getMonth() + i);
+        oldReportDate.setMonth(oldReportDate.getMonth() + i);
         newLine[pos_report_date] = oldReportDate.format("yyyy.MM.dd");
         compute_total_money(newLine);
         gRows.push(newLine.join(','));
@@ -107,12 +90,32 @@ for (var i = 0; i < lines.length; i++) {
 }
 
 console.log("--- 收益 ---")
-console.log(gRows)
+console.log(gRows);
+//
+write_gains_csv();
 
-function printHeader(lines) {
+
+
+//收益表
+function write_gains_csv() {
+    write_csv = require('./utils').write_csv;
+    var d = new Date();
+    gRows.unshift(headerLine);
+    write_csv(gRows, '收益-' + d.getTime() + '.csv');
+}
+
+
+function extractHeader(lines) {
     lines[0].split(',').forEach(function (v, idx) {
-        console.log(idx + ':"' + v + '",')
-    })
+        // console.log(idx + ':"' + v + '",')
+        header[idx] = (v.trim())
+    });
+}
+function printHeader(lines) {
+    console.log("header:");
+    for (i in header){
+        console.log(i+":"+header[i]);
+    }
 }
 
 function extend_Date() {
@@ -122,7 +125,7 @@ function extend_Date() {
 // 例子：
 // (new Date()).Format("yyyy-MM-dd hh:mm:ss.S") ==> 2006-07-02 08:09:04.423
 // (new Date()).Format("yyyy-M-d h:m:s.S")      ==> 2006-7-2 8:9:4.18
-    Date.prototype.Format=Date.prototype.format = function (fmt) { //author: meizz
+    Date.prototype.Format = Date.prototype.format = function (fmt) { //author: meizz
         var o = {
             "M+": this.getMonth() + 1, //月份
             "d+": this.getDate(), //日
